7cd1638a94c5eeb9ca5108f955e9b73ff2399267
Rachel Hanna <38994036+rjoyhanna@users.noreply.github.com>
2020-01-18 17:19:33 -0800

    added pagination

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './test';
+import { BrowserRouter } from 'react-router-dom'
 import * as serviceWorker from './serviceWorker';
+import { Router, Route, Link, browserHistory, IndexRoute } from 'react-router'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render((
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+), document.getElementById('root'))
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/test.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import Contact from './Contact';
+import './App.css';
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.OpenMenu = this.OpenMenu.bind(this);
+    this.CloseMenu = this.CloseMenu.bind(this);
+
+    this.state = {
+      menu: "closed"
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.Header()}
+        <main>
+          <p>main text</p>
+        </main>
+      </div>
+    );
+  }
+
+  OpenMenu(props) {
+    if (this.state.menu === "closed") {
+      this.setState({
+        menu: "open"
+      })
+    }
+  }
+
+  CloseMenu(props) {
+    if (this.state.menu === "open") {
+      this.setState({
+        menu: "closed"
+      })
+    }
+  }
+
+  render() {
+    return (
+    <Router>
+        <div>
+        <header>
+          {this.state.menu === "closed" ?
+            <img className="menu-icon" alt="open menu button" src="images/open_menu_white.png" onClick={this.OpenMenu}/>
+            :
+            <img className="menu-icon" alt="close menu button" src="images/close_menu_green.png" onClick={this.CloseMenu}/>
+          }
+          <h2>missing link</h2>
+        </header>
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <ul className="navbar-nav mr-auto">
+            <li><Link to={'/'} className="nav-link"> Home </Link></li>
+            <li><Link to={'/contact'} className="nav-link">Contact</Link></li>
+            <li><Link to={'/about'} className="nav-link">About</Link></li>
+          </ul>
+          </nav>
+          <hr />
+          <Switch>
+              <Route exact path='/' component={Home} />
+              <Route path='/contact' component={Contact} />
+              <Route path='/about' component={About} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+}
